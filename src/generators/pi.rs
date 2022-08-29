18083afce704673c4fa422a761bbbc258f889209
csfore <contactme@chrisfore.us>
2022-08-29 17:29:35 -0400

    Fix spacing

--- src/generators/pi.rs
+++ src/generators/pi.rs
@@ -3,7 +3,7 @@
 
     Python has a library called `decimal` that let's you calculate to the Nth decimal, while Rust is
     confined to f32 and f64. For now I'll keep this here in case we do find something in the future.
- */
+*/
 
 pub fn nth_pi(nth: i32) -> f64 {
     let mut c: f64 = 426880_f64 * 10005_f64.sqrt();
