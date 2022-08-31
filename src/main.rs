d06de55fef9798febd1de04bfd119c8e5e346839
csfore <contactme@chrisfore.us>
2022-08-31 15:00:51 -0400

    Add Discord functionality

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,58 @@ mod generators {
     pub mod fibo;
 }
 
-use std::io;
+use poise::serenity_prelude as serenity;
+use dotenv::dotenv;
 
-fn main() {
-    // This is all for debugging purposes
-    let mut floor = String::new();
-    let mut ceiling = String::new();
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+// User data, which is stored and accessible in all command invocations
+struct Data {}
 
-    println!("Enter your floor:");
-    io::stdin()
-        .read_line(&mut floor)
-        .expect("Didn't get that");
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
 
-    println!("Enter your ceiling: ");
-    io::stdin()
-        .read_line(&mut ceiling)
-        .expect("Didn't get that");
+/// Generates a random number between the floor and ceiling you provide
+#[poise::command(slash_command, prefix_command)]
+async fn num(
+    ctx: Context<'_>,
+    #[description = "Floor"] floor: Option<i32>,
+    #[description = "Ceiling"] ceiling: Option<i32>,
+) -> Result<(), Error> {
+    let f = floor.as_ref().unwrap_or_else(|| &1);
+    let c = ceiling.as_ref().unwrap_or_else(|| &100);;
+    ctx.say(generators::num::rand_num(*f, *c).to_string()).await?;
+    Ok(())
+}
 
-    let floor_parsed: i32 = floor.trim().parse().unwrap();
-    let ceiling_parsed: i32 = ceiling.trim().parse().unwrap();
+#[poise::command(prefix_command)]
+async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
 
-    let fibo = generators::fibo::nth_fibo(floor_parsed);
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
 
-    println!("Nth digit is: {}", fibo);
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            // if you get a red line here on the last parenthesis, ignore it
+            commands: vec![age(), register(), num()],
+            ..Default::default()
+        })
+        .token(std::env::var("TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
+
+    framework.run().await.unwrap();
 }
