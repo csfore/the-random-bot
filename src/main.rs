28b3d812aad23960cb22f5a1cd58f811a408b36a
Marshall P <mdpearson2@gmail.com>
2023-07-23 21:41:34 -0400

    Added word command & added negative numbers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use poise::serenity_prelude as serenity;
+use std::collections::HashMap;
 use rand::{
     Rng,
     seq::SliceRandom
@@ -35,14 +36,45 @@ async fn test(
     Ok(())
 }
 
+/// Random Word generator
+#[poise::command(prefix_command, slash_command)]
+async fn word(ctx: Context<'_>) -> Result<(), Error> {
+    let config_path = "dictionary.json";
+    let config_read = std::fs::read_to_string(&config_path);
+
+    let dictionary: HashMap<String, String> = serde_json::from_str(&config_read.expect("dictionary.json not found in the top level directory.")).expect("dictionary.json wasn't able to be parsed");
+
+    let mut keys: Vec<String> = Vec::new();
+    let mut values: Vec<String> = Vec::new();
+
+    for (key, value) in dictionary {
+        keys.push(key);
+        values.push(value);
+    }
+
+    let choice = rand::thread_rng().gen_range(0..keys.len() - 1);
+
+    let keys_ret = &keys[choice];
+    let values_ret = &values[choice];
+
+    ctx.send(|f| f
+        .embed(|f| f
+            .title("Your word is....")
+            .description(format!("{} - {}", keys_ret, values_ret))
+            .color(BRAND_COLOR))
+    ).await?;
+    Ok(())
+
+}
+
 /// Random Number Generator
 #[poise::command(prefix_command, slash_command)]
 async fn number(
     ctx: Context<'_>,
-    #[description = "The number to start at"] floor: Option<u128>,
-    #[description = "The number to end at"] ceiling: Option<u128>
+    #[description = "The number to start at"] floor: Option<i128>,
+    #[description = "The number to end at"] ceiling: Option<i128>
 ) -> Result<(), Error> {
-    let mut floor_raw = 0;
+    let mut floor_raw: i128 = 0;
     let mut ceiling_raw = 10;
 
     if let (Some(floor), Some(ceiling)) = (floor, ceiling) {
@@ -50,7 +82,7 @@ async fn number(
         ceiling_raw = ceiling;
     }
 
-    let num: u128 = rand::thread_rng().gen_range(floor_raw..=ceiling_raw);
+    let num: i128 = rand::thread_rng().gen_range(floor_raw..=ceiling_raw);
 
     ctx.send(|f| f
         .embed(|f| f
@@ -93,17 +125,22 @@ async fn ask(
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() {   
+
+    let config_file = std::fs::read_to_string("config.json").expect("config.json file not found in top directory"); 
+    let config_dict: HashMap<String, String> = serde_json::from_str(&config_file).expect("config.json couldn't be parsed. Check the file format"); 
+
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: vec![
                 test(),
                 number(),
-                ask()
+                ask(),
+                word()
             ],
             ..Default::default()
         })
-        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .token(config_dict.get("DISCORD_TOKEN").expect("missing DISCORD_TOKEN in config.json"))
         .intents(serenity::GatewayIntents::non_privileged())
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
