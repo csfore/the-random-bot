864b1a632146569f988f29cdb430fba4b7008363
Christopher Fore <csfore@posteo.net>
2023-07-23 20:18:54 -0400

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,15 @@ async fn ask(
 ) -> Result<(), Error> {
     let mut answer = "";
 
-    let questions: Vec<&str> = vec!["It is certain.", "Reply hazy, try again", "Don't count on it", "It is decidedly so", "Ask again later", "My reply is no", "Without a doubt", "Better not tell you now", "My sources say no", "Yes, definitely", "Cannot predict now", "Outlook not so good", "You may rely on it", "Concentrate and ask again", "Very doubtful", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"];
+    let questions: Vec<&str> = vec![
+        "It is certain.", "Reply hazy, try again", 
+        "Don't count on it", "It is decidedly so", "Ask again later", 
+        "My reply is no", "Without a doubt", "Better not tell you now", 
+        "My sources say no", "Yes, definitely", "Cannot predict now", 
+        "Outlook not so good", "You may rely on it", "Concentrate and ask again",
+        "Very doubtful", "As I see it, yes", "Most likely", "Outlook good", "Yes",
+        "Signs point to yes"
+    ];
 
     if let Some(answer_raw) = questions.choose(&mut rand::thread_rng()) {
         answer = *answer_raw;
